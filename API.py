59ec21c7286fd468a3f90253786cc6d7a0be8ac4
Paul Teufel <Paul@MacBook-Pro-104.local>
2020-03-21 14:37:09 +0100

    new data in data folder

--- API.py
+++ API.py
@@ -1,4 +1,6 @@
 from arcgis.gis import GIS
-gis = GIS("http://www.arcgis.com/home/webmap/viewer.html?url=https://services7.arcgis.com/mOBPykOjAyBO2ZKk/ArcGIS/rest/services/RKI_Landkreisdaten/FeatureServer/0/&source=sd", "pteufel", "W59echawo!o5")
+gis = GIS("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/ArcGIS/rest/services/Corona_Gemeinden/FeatureServer/0", "pteufel", "W59echawo!o5")
 print(f"Connected to {gis.properties.portalHostname} as {gis.users.me.username}")
 
+f= gis.content.get('ObjectID')
+print(f.layers[0])
