73828fc6cb03689c1aba2914eb4d3fb3c1323a00
paulttt <58082819+paulttt@users.noreply.github.com>
2020-03-28 14:59:09 +0100

    Delete API.py

--- API.py
+++
@@ -1,6 +0,0 @@
-from arcgis.gis import GIS
-gis = GIS("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/ArcGIS/rest/services/Corona_Gemeinden/FeatureServer/0", "pteufel", "W59echawo!o5")
-print(f"Connected to {gis.properties.portalHostname} as {gis.users.me.username}")
-
-f= gis.content.get('ObjectID')
-print(f.layers[0])
